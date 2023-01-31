34cd65f5073abe1be7841f3422cbbcfa1153e475
Appinessh <119859162+Appinessh@users.noreply.github.com>
2023-02-01 02:05:24 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	int* array = (int*)malloc(sizeof(int) * n);
+	int* dp = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &array[i]);
+		dp[i] = 1;
+	}
+	dp[0] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (array[j] < array[i])
+			{
+				dp[i] = (dp[j] + 1) > dp[i] ? (dp[j] + 1) : dp[i];
+			}
+		}
+	}
+	int max = INT_MIN;
+	for (int i = 0; i < n; i++)
+	{
+		max = max > dp[i] ? max : dp[i];
+	}
+	printf("%d", max);
+	return 0;
+}
