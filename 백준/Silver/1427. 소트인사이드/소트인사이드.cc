84cda843a7e117abe48a5fc2858cb74682f17859
Appinessh <119859162+Appinessh@users.noreply.github.com>
2023-02-25 03:14:55 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> v(20);
+	int cnt = 1;
+	while (n!= 0)
+	{
+		v[cnt - 1] = (n % (int)pow(10, cnt))/(int)pow(10,cnt-1);
+		n = n - n % (int)pow(10, cnt);
+		cnt++;
+	}
+	sort(v.begin(), v.end());
+	reverse(v.begin(), v.end());
+	for (int i = 0; i < cnt-1; i++)
+	{
+		cout << v[i];
+	}
+	return 0;
+}
