11843583ea77dcf2fade1f40194e5657b5a3aa31
Appinessh <119859162+Appinessh@users.noreply.github.com>
2023-06-28 23:13:34 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4852 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> arr;
+	arr.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\n";
+	}
+	return 0;
+}
