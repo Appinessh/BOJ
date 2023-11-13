8e0578009a5c4b4c58f92a5ffa0a196c810a07ea
Appinessh <119859162+Appinessh@users.noreply.github.com>
2023-11-13 22:44:15 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+int n, m;
+vector<int> lst;
+vector<int> sorted_lst;
+vector<int> ans;
+void get_ans(int depth)
+{
+	if (ans.size() == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = depth; i < sorted_lst.size(); i++)
+	{
+		ans.push_back(sorted_lst[i]);
+		get_ans(i);
+		ans.pop_back();
+	}
+	return;
+}
+int main(void)
+{
+	cin >> n >> m;
+	lst.resize(n);
+	set<int> s;
+	set<int>::iterator sit;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> lst[i];
+		s.insert(lst[i]);
+	}
+	for (sit=s.begin();sit!=s.end();sit++)
+	{
+		sorted_lst.push_back(*sit);
+	}
+	sort(sorted_lst.begin(), sorted_lst.end());
+	get_ans(0);
+	return 0;
+}
